2407fc3e5236465409e135bbd0a3c0058297467a
Roman V. Yudanov <Alaricy@gmail.com>
2022-02-14 21:18:54 +0200

    урл пока работают

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('group/', views.group_posts),
+    path('group/<str:any_slag>/', views.group_posts1)
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request):
-    return HttpResponse('Посты Толстого')   
+    template = 'posts/group_list.html'
+    return render(request, template)
+
+def group_posts1(request, any_slag):
+    return HttpResponse(f'Посты Толстого {any_slag}')
