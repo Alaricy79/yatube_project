d9dd7ed3e152a5aa62724c28e5bdc7385e5ef459
Roman V. Yudanov <Alaricy@gmail.com>
2022-02-15 15:55:35 +0200

    добрался до static

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
 from . import views
+app_name = 'posts'
+
+
 
 urlpatterns = [
-    path('', views.index),
-    path('group/', views.group_posts),
+    path('', views.index, name='home'),
+    path('group/', views.group_posts, name='posts'),
     path('group/<str:any_slag>/', views.group_posts1)
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context = {
+        'title': 'главная страница проекта YaTube',
+        'text': 'Это главная страница проекта YaTube',
+    }
+
+
     template = 'posts/index.html'
-    return render(request, template)
+    return render(request, template, context)
 
 def group_posts(request):
+    context = {
+        'title': 'информация о группах проекта Yatube',
+        'text': 'Здесь будет информация о группах проекта Yatube',
+    }
+
     template = 'posts/group_list.html'
-    return render(request, template)
+    return render(request, template, context)
 
 def group_posts1(request, any_slag):
     return HttpResponse(f'Посты Толстого {any_slag}')
