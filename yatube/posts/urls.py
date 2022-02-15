7581f3f519f3318a8f840d0270bd077d0f5f57ca
Roman V. Yudanov <Alaricy@gmail.com>
2022-02-15 21:26:49 +0200

    начал финальное задание. не работает.

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -8,5 +8,5 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='home'),
     path('group/', views.group_posts, name='posts'),
-    path('group/<str:any_slag>/', views.group_posts1)
+    path('group/<slug>/', views.group_posts1)
 ] 
